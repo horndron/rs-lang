f2b8b01e0aea13dfb566f1f4d6a0b4e91e51b7df
horndron <horndron@yandex.ru>
2022-02-03 14:41:43 +0300

    feat: add api method

---
+++ src/interfaces/api.ts
@@ -0,0 +1,54 @@
+export interface Word {
+  id: string
+  group: number
+  page: number
+  word: string
+  image: string
+  audio: string
+  audioMeaning: string
+  audioExample: string
+  textMeaning: string
+  textExample: string
+  transcription: string
+  textExampleTranslate: string
+  textMeaningTranslate: string
+  wordTranslate: string
+}
+
+export interface User {
+  email: string
+  password?: string
+  name?: string
+  id?: string
+}
+
+export interface AuthUser {
+  message: string
+  token: string
+  refreshToken: string
+  userId: string
+  name: string
+}
+
+export interface RejectStatusText {
+  status: number
+  message: string
+}
+
+export interface Options {
+  [key: string]: string | number | boolean
+}
+
+export interface UserWordParams {
+  difficulty: string
+  optional: Options
+}
+
+export interface UserWord {
+  difficulty: string
+  optional: Options
+  id: string
+  wordId: string
+}
+
+export default Word
