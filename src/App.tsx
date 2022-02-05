7bbf7342bca3142578a75f4ddc2976df8e5eafb8
buterbrot359 <batowka359@gmail.com>
2022-02-05 21:15:35 +0600

    feat: delete Counter && add Browser Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
-import { Counter } from './Counter'
+import { NavLink, Routes, HashRouter, Route } from 'react-router-dom'
 
 export const App = () => {
   return (
     <>
-      <h1>React TypeScript Webpack Starter Template</h1>
-      <Counter />
+      <HashRouter>
+        <header className="header-container">
+          <nav className="nav-bar">
+            <ul className="nav-list">
+              <li className="nav-btn">
+                <NavLink to="/">Главная</NavLink>
+              </li>
+              <li className="nav-btn">
+                <NavLink to="/textbook">Учебник</NavLink>
+              </li>
+              <li className="nav-btn">
+                <NavLink to="/games">Игры</NavLink>
+              </li>
+              <li className="nav-btn">
+                <NavLink to="/stats">Статистика</NavLink>
+              </li>
+              <li className="nav-btn">
+                <NavLink to="/about">О нас</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main className="main-container">
+          <Routes>
+            <Route path={'/'}></Route>
+            <Route path={'/textbook'}></Route>
+            <Route path={'/games'}></Route>
+            <Route path={'/stats'}></Route>
+            <Route path={'/about'}></Route>
+          </Routes>
+        </main>
+        <footer className="footer-container"></footer>
+      </HashRouter>
     </>
   )
 }
