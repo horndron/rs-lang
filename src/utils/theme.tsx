8bc3b655919b4e6e5b1b688fdce0fc227014bb4c
nat6 <nat6dv@gmail.com>
2022-02-08 22:04:27 +0400

    feat: add basic theme

---
+++ src/utils/theme.tsx
@@ -0,0 +1,39 @@
+import { createTheme } from '@mui/material/styles'
+
+export const theme = createTheme({
+  palette: {
+    // * PolyLang theme colors
+    // primary: {
+    //   main: '#FE5244',
+    // },
+    // secondary: {
+    //   main: '#FE7367',
+    // },
+    // background: {
+    //   default: '#FFF1F0',
+    // },
+    primary: {
+      main: '#4E97C2',
+      light: '#95bdd4',
+    },
+    secondary: {
+      main: '#026AA7',
+    },
+    background: {
+      default: '#284968',
+    },
+    text: {
+      primary: '#ecf7ff',
+    },
+  },
+  typography: {
+    fontFamily: 'Titillium Web, Arial',
+    h1: {
+      fontFamily: 'Ribeye Marrow, cursive',
+      fontSize: '1.2rem',
+    },
+    button: {
+      fontSize: '0.9rem',
+    },
+  },
+})
