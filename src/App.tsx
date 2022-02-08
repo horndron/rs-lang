a672c5ed928855605b757e58b93967525604679e
nat6 <nat6dv@gmail.com>
2022-02-08 22:21:53 +0400

    feat: include header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,31 @@
-import { NavLink, Routes, HashRouter, Route } from 'react-router-dom'
+import { Routes, HashRouter, Route } from 'react-router-dom'
+import { ThemeProvider } from '@mui/material/styles'
+import { theme } from './utils/theme'
 import './App.styles.scss'
+
+import Header from './components/header/header'
+import Footer from './components/footer/footer'
+
 import { TextbookPage } from './components/textbook/textbook'
 
 export const App = () => {
   return (
     <>
       <HashRouter>
-        <header className="header-container">
-          <nav className="nav-bar container">
-            <ul className="nav-list">
-              <li className="nav-item">
-                <NavLink to="/">Главная</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/textbook">Учебник</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/games">Игры</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/stats">Статистика</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/about">О нас</NavLink>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        <main className="main-container">
-          <Routes>
-            <Route path={'/'}></Route>
-            <Route path={'/textbook'} element={<TextbookPage />}></Route>
-            <Route path={'/games'}></Route>
-            <Route path={'/stats'}></Route>
-            <Route path={'/about'}></Route>
-          </Routes>
-        </main>
-        <footer className="footer-container"></footer>
+        <ThemeProvider theme={theme}>
+          <Header />
+          <main className="main-container">
+            <Routes>
+              <Route path={'/'}></Route>
+              <Route path={'/textbook'} element={<TextbookPage />}></Route>
+              <Route path={'/games'}></Route>
+              <Route path={'/stats'}></Route>
+              <Route path={'/about'}></Route>
+            </Routes>
+          </main>
+          {/* // todo: remove footer for games pages */}
+          <Footer />
+        </ThemeProvider>
       </HashRouter>
     </>
   )
