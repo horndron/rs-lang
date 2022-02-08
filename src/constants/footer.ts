67ea05127023ca6017c47e3295ec6c567326c81e
nat6 <nat6dv@gmail.com>
2022-02-08 22:14:58 +0400

    feat: add footer

---
+++ src/constants/footer.ts
@@ -0,0 +1,16 @@
+export const AUTHORS = [
+  {
+    name: 'mairs',
+    link: 'https://github.com/horndron',
+  },
+  {
+    name: 'buterbrot',
+    link: 'https://github.com/ButerBrot359',
+  },
+  {
+    name: 'nat',
+    link: 'https://github.com/nat6',
+  },
+]
+
+export const COURSE_LINK = 'https://rs.school/js/'
