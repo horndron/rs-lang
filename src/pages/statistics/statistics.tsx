4bf679c716bbd30b6fd24fdfd4cd3e4ab9c5cf1a
horndron <horndron@yandex.ru>
2022-02-21 21:13:33 +0300

    feat: add StatisticForDay component

--- src/pages/statistics/statistics.tsx
+++ src/pages/statistics/statistics.tsx
@@ -1,31 +1,53 @@
 import { Typography } from '@mui/material'
-import React from 'react'
+import React, { FC, useEffect } from 'react'
+import { getUserStatistics } from './../../components/APIs/api'
 
-export const Statistics = () => {
-  const canvas = document.querySelector('#canvas') as HTMLCanvasElement
-  console.log(canvas)
-  // const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
-  // ctx.fillStyle = 'black'
-  // ctx.lineWidth = 2.0
-  // ctx.beginPath()
-  // ctx.moveTo(30, 10)
-  // ctx.lineTo(30, 460)
-  // ctx.lineTo(500, 460)
-  // ctx.stroke()
+export const Statistics: FC = () => {
+  const statisticData = async (): Promise<void> => {
+    const userId = localStorage.getItem('userId') as string
+    const token = localStorage.getItem('token') as string
+    const statistic = await getUserStatistics(userId, token)
 
-  // ctx.fillStyle = 'black'
-  // for (let i = 0; i < 6; i++) {
-  //   ctx.fillText((5 - i) * 20 + '', 4, i * 80 + 60)
-  //   ctx.beginPath()
-  //   ctx.moveTo(25, i * 80 + 60)
-  //   ctx.lineTo(30, i * 80 + 60)
-  //   ctx.stroke()
-  // }
-  // const labels = ['JAN', 'FEB', 'MAR', 'APR', 'MAY']
+    console.log(statistic)
 
-  // for (let i = 0; i < 5; i++) {
-  //   ctx.fillText(labels[i], 50 + i * 100, 475)
-  // }
+    const canvas = document.querySelector('#canvas') as HTMLCanvasElement
+    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+    const canvasWidth = canvas.clientWidth
+    const canvasHeight = canvas.clientHeight
+    const scaleX = 50
+    const scaleY = 50
+    ctx.beginPath()
+    ctx.strokeStyle = '#eee'
+
+    for (let i = 0; i < canvasWidth; i = i + scaleX) {
+      ctx.moveTo(i, 0)
+      ctx.lineTo(i, canvasHeight)
+    }
+
+    for (let i = 0; i < canvasHeight; i = i + scaleY) {
+      ctx.moveTo(0, i)
+      ctx.lineTo(canvasWidth, i)
+    }
+    ctx.stroke()
+    ctx.closePath()
+
+    const xAxis = canvasWidth
+    const yAxis = canvasHeight
+
+    ctx.beginPath()
+    ctx.strokeStyle = '#026aa7'
+    ctx.moveTo(0, yAxis)
+    ctx.lineTo(xAxis, yAxis)
+
+    ctx.moveTo(0, yAxis)
+    ctx.lineTo(0, 0)
+    ctx.stroke()
+    ctx.closePath()
+  }
+
+  useEffect(() => {
+    statisticData()
+  }, [])
 
   return (
     <div className="statistics-page">
@@ -33,28 +55,10 @@ export const Statistics = () => {
         <div>
           <h1>Статистика за сегодня</h1>
           <div className="statistics">
-            <div className="statistics_day">
-              <div className="day_words"></div>
-              <div className="day_right_answers"></div>
-            </div>
-            <div className="statistics_games">
-              <div className="game_audiocall">
-                <h3>Аудиовызов</h3>
-                <div className="words">Изучено слов</div>
-                <div className="right-answer">Правильных ответов: .</div>
-                <div className="longest-series">Лучшая серия: .</div>
-              </div>
-              <div className="game_sprint">
-                <h3>Спринт</h3>
-                <div className="words">Изучено слов</div>
-                <div className="right-answer">Правильных ответов: .</div>
-                <div className="longest-series">Лучшая серия: .</div>
-              </div>
-            </div>
             <div className="statistics_all_time">
               <h2>Статистика за всё время</h2>
               <div className="canvas-container">
-                <canvas width="750" height="400" id="canvas" />
+                <canvas width="750" height="350" id="canvas" />
               </div>
             </div>
           </div>
