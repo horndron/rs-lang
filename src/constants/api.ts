6afbe9ef0daf8aeb6fa0c2c139b3779d3ad4fef1
Andrey Filippov <89828396+horndron@users.noreply.github.com>
2022-03-17 18:15:12 +0300

    Revert "Main page adaptive"

--- src/constants/api.ts
+++
@@ -1,9 +0,0 @@
-export const BACKEND_HOSTNAME = 'https://react-learnwords-36.herokuapp.com'
-export const URL_WORDS = 'words'
-export const URL_USERS = 'users'
-export const URL_SIGNIN = 'signin'
-export const URL_TOKENS = 'tokens'
-export const URL_STATISTICS = 'statistics'
-export const URL_AGREGATED = 'aggregatedWords'
-
-export default BACKEND_HOSTNAME
